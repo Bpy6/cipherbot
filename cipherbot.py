65ea809908e17717b1cd5707ce6ea3e564eca3aa
Bpy6 <56440340+Bpy6@users.noreply.github.com>
2020-08-19 01:30:02 +0300

    Update cipherbot.py

--- cipherbot.py
+++ cipherbot.py
@@ -2,187 +2,145 @@ import requests
 import random
 import time
 import json
-import urllib
-import webbrowser
-import io
-import base64
 
 token = '1017242146:AAEnjzBDXaWFFeg3KSLCTla0SMUGs-O5fOw'
 url = 'https://api.telegram.org/bot'
-chat_id = '-1001202697355'
-bpy6_chat = '228334796'
-cipherPts = 1
 
-class Cipher:
-    def __init__(self, currentCipher, answer):
-        self.currentCipher = currentCipher
-        self.answer = answer
+class Riddle:
+    def __init__(self):
+        self.scoreboard = {}
         self.winners = []
-        self.points = cipherPts
+        self.reward = 2
+        self.status = 'setup'
+        self.question = self.getQuestion()
+        print(f'Accepted Question:{self.question}')
+        self.correctAnswer = self.getAnswer()
+        print(f'Accepted Answer:{self.correctAnswer}')
+        self.run()
+        
+    def getQuestion(self):
+        bot.sendMessage(228334796, 'Ну, запиливай!')
+        while True:
+            update = Update(bot.getOneUpdate(bot.offset))
+            if update.sender == 'Bpy6na': return update.message_id
+                
+    def getAnswer(self):
+        bot.sendMessage(228334796, 'Теперь ответ')
+        while True:
+            update = Update(bot.getOneUpdate(bot.offset))
+            if update.sender == 'Bpy6na': return update.text
+
     def run(self):
-        bot.forwardMessage(bpy6_chat, chat_id, self.currentCipher)
-        bot.sendMessage(chat_id, 'Напиши "шифр", и я повторю задание.')
-        bot.sendMessage(chat_id, 'А ответы отправляй мне в личку!')
-
+        bot.forwardMessage(228334796, -1001202697355, self.question)
+        bot.sendMessage(-1001202697355, 'Напиши "шифр", и я повторю задание.')
+        bot.sendMessage(-1001202697355, 'А ответы отправляй мне в личку!')
+        self.status = 'running'
+        
+    def reduceReward(self, winner):
+        
+        self.winners.append(winner)
+        if winner not in self.scoreboard.keys(): self.scoreboard.update({winner:0})
+        print(self.scoreboard)
+        self.scoreboard.update({winner:+self.reward})
+        print(self.scoreboard)
+        self.reward -= 1
+        if self.reward == 0: self.close()
+        
     def close(self):
-        global scoreboard
-        (self.currentCipher, self.answer) = ('new','new')
-        bot.sendMessage(chat_id, 'Раунд завершен! Победители:')
-        for winner in self.winners: bot.sendMessage(chat_id, winner)
+        self.status = 'setup'
+        bot.sendMessage(-1001202697355, 'Шифр разгадан! Победители:')
+        for winner in self.winners: bot.sendMessage(-1001202697355, winner)
+        bot.sendMessage(-1001202697355, f'Итоговый зачет:\n{str(self.scoreboard)}')
         self.winners = []
-        bot.sendMessage(chat_id, str(scoreboard))
-        self.points = cipherPts    
-    
+
+class Update:
+    def __init__(self, result):
+        self.result = result
+        self.message_id = self.result.get('message','').get('message_id','')
+        self.text = self.result.get('message','').get('text','').lower()
+        if self.result.get('message','').get('new_chat_participant',{}).get('username',''):
+            self.newMemberName = self.result.get('message','').get('new_chat_participant',{}).get('username','')
+        else: self.newMemberName = self.result.get('message','').get('new_chat_participant',{}).get('first_name','')
+        if self.result.get('message','').get('from',{}).get('username',''):
+            self.sender = self.result.get('message','').get('from',{}).get('username','')
+        else: self.sender = self.result.get('message','').get('from',{}).get('first_name','')
+        self.chatType = self.result.get('message','').get('chat',{}).get('type','')
+        self.chat_id = self.result.get('message','').get('chat',{}).get('id','')
+        print(self.result.get('message'))  
+           
 class CipherBot:
     def __init__(self, ready, token):
         self.ready = False
         self.token = token
-        file = open('update_id.txt','r')
-        self.offset = int(file.readline())
-        file.close()
-    
-    def update(self):
-        call = requests.get('https://api.telegram.org/bot1017242146:AAEnjzBDXaWFFeg3KSLCTla0SMUGs-O5fOw/getUpdates?offset='+str(self.offset))
-        update = call.json().get('result')
-        if update:
-            self.offset = int(update[-1].get('update_id'))+1
-            file = open('update_id.txt','w')
-            file.write(str(self.offset))
-            file.close()
-        if update: print(update)
-        return update
+        self.offset = int(self.getInitialOffset())+1
         
-    def checkForEvents(self, update):
-        global scoreboard
-        for event in update:
-            print(riddle.answer.encode('utf-8'))      
-            if 'text' in event.get('message'):
-                if event.get('message').get('text').lower() == 'шифр' and event.get('message').get('from').get('is_bot') == False:
-                    bot.forwardMessage(bpy6_chat, chat_id, riddle.currentCipher)
-                if event.get('message').get('chat').get('type') == 'private':
-                    self.addToScoreboard(event.get('message').get('from').get('username'))
-                    attempt = event.get('message').get('text').lower()
-                    if str(base64.b64encode(attempt.encode('utf-8'))) == riddle.answer:
-                        if event.get('message').get('from').get('username') not in riddle.winners:
-                            riddle.winners.append(event.get('message').get('from').get('username'))
-                            scoreboard.update({event.get('message').get('from').get('username'):scoreboard.get(event.get('message').get('from').get('username'))+riddle.points})
-                            riddle.points -= 1
-                            self.sendMessage(self.getChatId(event), 'Молодей! Победа!')
-                        else: self.sendMessage(self.getChatId(event), 'Эээ! Тебе за это задание уже заправили!')
-                    else: self.sendMessage(self.getChatId(event), 'Ноуп')
-                                               
-                print(event.get('message').get('from').get('username') + ': ' + event.get('message').get('text'))
-                       
-            if 'new_chat_participant' in event.get('message'):
-                if event.get('message').get('new_chat_participant').get('username'):
-                    username = event.get('message').get('new_chat_participant').get('username')
-#                    chatid = event.get('message').get('chat').get('id')
-                    chatid = '-1001202697355'
-                    print(chatid)
-                    self.sendMessage(self.getChatId(event), f'Доброе утро, {username}!' ) 
-                    bot.sendMessage(chat_id, 'Напиши "шифр", и я повторю задание.')
-                    bot.sendMessage(chat_id, 'А ответы отправляй мне в личку!')
-    
-    def addToScoreboard(self, username):
-        global scoreboard
-        if username not in scoreboard.keys(): 
-            scoreboard.update({username:0})
-            print(username+' added to scoreboard')
-        else: print(scoreboard)
-            
     def getEndpoint(self, endpoint):
         self.endpoint = endpoint
         
     def buildSignature(self):
         self.url = url + self.token + self.endpoint
         
-    def getChatId(self, event):
-        chatId = str(event.get('message').get('chat').get('id'))
-        return chatId
+    def getInitialOffset(self):
+        offset = self.getOneUpdate('None').get('update_id')
+        print(f'First update received. Update ID: {offset}')
+        return offset    
     
-    def forwardMessage(self, fromChatId, toChatId, messageId):
-        self.getEndpoint('/forwardMessage')
-        self.buildSignature()
-        call = requests.post(self.url, data={'chat_id':toChatId,'from_chat_id':fromChatId, 'message_id':messageId})
-        print(call.text)
+    def getOneUpdate(self, offset):
+        updates = None
+        print('awaiting update...')
+        while not updates:
+            time.sleep(1)
+            call = requests.get('https://api.telegram.org/bot1017242146:AAEnjzBDXaWFFeg3KSLCTla0SMUGs-O5fOw/getUpdates?offset='+str(offset))
+            updates = call.json().get('result')
+        self.offset = int(updates[-1].get('update_id'))+1
+        return updates[-1]
+    
+    def getUpdates(self):
+        call = requests.get('https://api.telegram.org/bot1017242146:AAEnjzBDXaWFFeg3KSLCTla0SMUGs-O5fOw/getUpdates?offset='+str(self.offset))
+        result = call.json().get('result')
+        if result:
+            self.offset = int(result[-1].get('update_id'))+1
+            return result
         
-    def sendAnimation(self, event):
-        self.getEndpoint('/sendAnimation')
-        self.buildSignature()
-        call = requests.post(self.url, data={'chat_id':self.getChatId(event),'animation':self.getKielbasa()})
-        print(call.text)
-    
+    def checkForEvents(self, update):
+        if update.chatType == 'private' and update.text == activeRiddle.correctAnswer:
+            if update.sender not in activeRiddle.winners: 
+                self.sendMessage(update.chat_id, 'Молодей! Победа!')
+                activeRiddle.reduceReward(update.sender)
+            else: self.sendMessage(update.chat_id, 'Тебе за это задание уже заправили!')
+            
+        elif update.chatType == 'private': self.sendMessage(update.chat_id, 'Ноуп')
+            
+        if update.chatType in ('group','supergroup'):
+            if update.text == 'шифр' and activeRiddle.status == 'running': activeRiddle.run()
+                
+        if update.newMemberName:
+            self.sendMessage(update.chat_id, f'Привет, {update.newMemberName}')
+            if activeRiddle.status == 'running': activeRiddle.run()
+            
+        
     def sendMessage(self, chatId, text):
         self.getEndpoint('/sendMessage')
         self.buildSignature()
         call = requests.post(self.url, data={'chat_id':chatId,'text':text})
-        print(call.text)
-
-def getNewCipher(currentCipher, answer):
-    if (currentCipher, answer) == ('new','new'):
-        bot.sendMessage(bpy6_chat, 'Давай-ка, запили мне новый шифр!')
-        received = False
-        while not received:
-            try:
-                time.sleep(1)
-                for event in bot.update():
-                    if event.get('message').get('chat').get('id') == 228334796: currentCipher = event.get('message').get('message_id')
-                    print(currentCipher)
-                    received = True
-            except TypeError:
-                print('wtf try again')
-        bot.sendMessage(bpy6_chat, 'Отличненько! Теперь дай ответ')
-        received = False
-        while not received:
-            try:
-                time.sleep(1)
-                for event in bot.update():
-                    if event.get('message').get('chat').get('id') == 228334796: answer_raw = event.get('message').get('text')
-                    answer = str(base64.b64encode(answer_raw.encode('utf-8')))
-                    print(answer)
-                    received = True
-            except TypeError:
-                print('wtf try again')
-        bot.sendMessage(bpy6_chat, 'Ну хорошо, давай врубим это дело!')
-    riddle = Cipher(currentCipher, str(answer))
-    return riddle
+        print(f'Sent to {chatId}: {text}')
+        
+    def forwardMessage(self, fromChatId, toChatId, messageId):
+        self.getEndpoint('/forwardMessage')
+        self.buildSignature()
+        call = requests.post(self.url, data={'chat_id':toChatId,'from_chat_id':fromChatId, 'message_id':messageId})
+        print(f'Forwarded message {messageId} to {toChatId}')
 
 if __name__ == '__main__':
     bot = CipherBot(False, token)
-    with open('cipher.txt', 'r') as f:
-        currentCipher = f.readline().strip()
-        answer = f.readline().strip()
-    print(currentCipher, answer)
-    riddle = getNewCipher(currentCipher, answer)
-    riddle.run()
-    
-    with open('scoreboard.txt', 'r') as f:
-        scoreboard = dict(eval(f.read()))
-        print(scoreboard)
-    with open('winners.txt', 'r') as f:
-        riddle.winners = eval(f.read())
-        print(riddle.winners)
-    with open('currentpoints.txt', 'r') as f:
-        riddle.points = int(f.read())
-        print(riddle.points)
-    
     while True:
-        try:
-            time.sleep(1)
-            bot.checkForEvents(bot.update())
-            if riddle.points == 0: 
-                riddle.close()
-                break
-        except TypeError:
-            print('TypeError exception occurred')
-        finally:
-            with open('scoreboard.txt', 'w') as f:
-                f.write(str(scoreboard))
-            with open('winners.txt', 'w') as f:
-                f.write(str(riddle.winners))
-            with open('currentpoints.txt', 'w') as f:
-                f.write(str(riddle.points))
-            with open('cipher.txt', 'w') as f:
-                f.write(str(riddle.currentCipher))
-                f.write('\n')
-                f.write(str(riddle.answer))
+        activeRiddle = Riddle()
+        while activeRiddle.status == 'running':
+            try:
+                time.sleep(1)
+                bot.update = Update(bot.getUpdates()[-1])
+                bot.checkForEvents(bot.update)
+            except TypeError:
+                pass
+       
+    
